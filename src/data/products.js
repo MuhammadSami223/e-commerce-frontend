f3c00ad81c490234225349202eacc6547613b01b
MuhammadSami223 <Muhammadsamiahsan@gmail.com>
2023-08-30 02:45:54 -0700

    demo changes

---
+++ src/data/products.js
@@ -0,0 +1,52 @@
+
+export const products=[
+
+{
+     productName:"Nikee shoes",
+     description:"Nike, Inc. is an American athletic footwear and apparel corporation headquartered near Beaverton, Oregon, United States. It is the world's largest supplier of athletic shoes and apparel and a major manufacturer of sports equipment, with revenue in excess of US$46 billion in its fiscal year 2022. ",
+     price:"450,000",
+     image:"https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&w=400",
+},
+{
+     productName:"Iphone 13",
+     description:"This is iphone13 description",
+     price:"420,000",
+     image:"https://images.pexels.com/photos/159886/pexels-photo-159886.jpeg?auto=compress&cs=tinysrgb&w=400",
+},
+{
+     productName:"Iphone 11",
+     description:"This is iphone11 description",
+     price:"460,000",
+     image:"https://images.pexels.com/photos/17788543/pexels-photo-17788543/free-photo-of-iphone.jpeg?auto=compress&cs=tinysrgb&w=400",
+},
+{
+     productName:"Iphone 12",
+     description:"Tis is iphone12 description",
+     price:"530,000",
+     image:"https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+},
+{
+     productName:"Iphone 14",
+     description:"THis is iphone description",
+     price:"450,000",
+     image:"https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&w=400",
+},
+{
+     productName:"Iphone 13",
+     description:"This is iphone13 description",
+     price:"420,000",
+     image:"https://images.pexels.com/photos/159886/pexels-photo-159886.jpeg?auto=compress&cs=tinysrgb&w=400",
+},
+{
+     productName:"Iphone 11",
+     description:"This is iphone11 description",
+     price:"460,000",
+     image:"https://images.pexels.com/photos/17788543/pexels-photo-17788543/free-photo-of-iphone.jpeg?auto=compress&cs=tinysrgb&w=400",
+},
+{
+     productName:"Iphone 12",
+     description:"Tis is iphone12 description",
+     price:"530,000",
+     image:"https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+},
+]
